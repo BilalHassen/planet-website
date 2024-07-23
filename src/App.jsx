fc4ded5142abc1f3331a79c962ae2c560ab7cb6d
BilalHassen <bilalhassen097@gmail.com>
2024-07-23 15:44:39 -0400

    add fonts and css font rules

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import "./App.scss";
 function App() {
   return (
     <>
-      <h1>Home</h1>
+      <h1>VENUS</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, sint
+        esse. Enim dignissimos quasi est molestias animi, ipsum veniam
+        voluptatum tenetur amet placeat adipisci cupiditate, architecto sit iste
+        debitis! Blanditiis?
+      </p>
     </>
   );
 }
