17f234e8a354c8ef77a4da8f0e9244d7cfdbd641
BilalHassen <bilalhassen097@gmail.com>
2024-08-08 10:26:56 -0400

    create PlanetsPage and add menu component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.scss";
 import Nav from "./components/Nav/Nav";
+import PlanetsPage from "./pages/PlanetsPage";
 
 function App() {
   return (
@@ -8,12 +9,7 @@ function App() {
       <Nav />
       <div className="wrapper">
         <h1>VENUS</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, sint
-          esse. Enim dignissimos quasi est molestias animi, ipsum veniam
-          voluptatum tenetur amet placeat adipisci cupiditate, architecto sit
-          iste debitis! Blanditiis?
-        </p>
+        <PlanetsPage />
       </div>
     </>
   );
---
+++ src/pages/PlanetsPage.jsx
@@ -0,0 +1,9 @@
+import Menu from "../components/Menu/Menu";
+
+export default function PlanetsPage() {
+  return (
+    <>
+      <Menu />
+    </>
+  );
+}
